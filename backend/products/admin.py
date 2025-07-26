3cbdfd7825bc28a8f076fe8819f6fc563b3505b1
amanzlastt <amanuelbizuayehu123@gmail.com>
2025-07-26 20:34:53 +0300

    products admin setup

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User, Product
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    **Objective**: Customize how User model appears in Django admin
+    **Key Concepts**:
+    - list_display: Fields to show in the list view
+    - search_fields: Fields to search by
+    - readonly_fields: Fields that can't be edited
+    """
+    list_display = ['email','first_name','last_name', 'date_joined']
+    search_fields = ['email', 'first_name', 'last_name']
+    readonly_fields = ['date_joined']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    **Objective**: Customize how Product model appears in Django admin
+    **Key Concepts**:
+    - list_display: Fields to show in the list view
+    - list_filter: Add filters in the sidebar
+    - search_fields: Fields to search by
+    """
+    list_display = ['name', 'type', 'price', 'created_at']
+    list_filter = ['type', 'created_at']
+    search_fields = ['name']
+    
